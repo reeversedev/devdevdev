24082af0b058b15aab16f9eccdf63216711c1453
Prateek Gogia <gogia.prateek@hotmail.com>
2020-03-27 17:57:03 +0530

    Able to render SVGs

--- components/explore.js
+++ components/explore.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import flash from '../icons/flash.svg';
 
 const Explore = () => {
   return (
-    <div>
+    <div className="explore">
       <h1>Explore</h1>
+      <ul>
+        {[
+          'New In',
+          'Clothing',
+          'Shoes',
+          'Accessories',
+          'Activewear',
+          'Gifts & Living',
+          'Inspiration'
+        ].map(li => {
+          return (
+            <li>
+              <a className="d-flex align-items-center">
+                <img src={flash} alt="icon" height="20" /> <span>{li}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
