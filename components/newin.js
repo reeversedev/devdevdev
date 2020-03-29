98a0ddee182530e8389339276a70dc3b5285bf5f
Prateek Gogia <gogia.prateek@hotmail.com>
2020-03-29 16:36:45 +0530

    sticky toolbar and scroll fixed

--- components/newin.js
+++ components/newin.js
@@ -4,10 +4,15 @@ import flash from '../icons/flash.svg';
 const Newin = () => {
   return (
     <div className="newin">
-      <div className="d-flex align-items-center">
-        <img src={flash} alt="icon" height="35" /> <h1>New In</h1>
+      <div className="d-flex align-items-center justify-content-between sticky-toolbar">
+        <div className="d-flex align-items-center">
+          <img src={flash} alt="icon" height="35" /> <h1>New In</h1>
+        </div>
+        <div className="d-flex justify-content-between align-items-center">
+          <button>Filter</button>
+        </div>
       </div>
-      <div className="d-flex">
+      <div className="d-flex highlights">
         <div className="w-50">
           <div className="category-horizontal-card cyan-background align-items-center text-center font-weight-800 font-size-22 m-1">
             GET UPTO 50% OFF
@@ -20,8 +25,10 @@ const Newin = () => {
           </div>
           <div className="d-flex">
             <div className="w-50">
-              <div className="category-vertical-card m-1">
-                <p>Adidas Originals LXCON 94 trainers in white</p>
+              <div className="category-vertical-card m-1 scarves">
+                <p>
+                  Blanket scarf in orange and black brushed check with tassels
+                </p>
               </div>
             </div>
             <div className="w-50">
