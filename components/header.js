ae9a7574b2bba3f160f000fa5c63b6d38c52d95e
Prateek Gogia <gogia.prateek@hotmail.com>
2020-04-02 23:52:08 +0530

    Finally mobile friendly

--- components/header.js
+++ components/header.js
@@ -3,12 +3,12 @@ import logo from '../icons/logo.svg';
 
 const Header = () => {
   return (
-    <div className="d-flex justify-content-between align-items-center header">
+    <div className="d-flex flex-sm-column justify-content-between align-items-center header">
       <div className="logo">
         <img src={logo} />
       </div>
 
-      <label className="search-bar ">
+      <label className="search-bar">
         <input
           type="text"
           placeholder="Search for awesome essentials..."
@@ -16,7 +16,7 @@ const Header = () => {
         />
       </label>
 
-      <div className="d-flex justify-content-between align-items-center">
+      <div className="d-flex justify-content-between align-items-center d-sm-none">
         <img
           src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"
           alt=""
@@ -26,7 +26,7 @@ const Header = () => {
         <span>Cart: </span>
         <span>2</span>
       </div>
-      <div className="d-flex">
+      <div className="d-flex d-sm-none">
         <div className="hello-name">
           <img
             src="https://pbs.twimg.com/profile_images/1239922488160575489/_Ykuf9DR_400x400.jpg"
--- components/newin.js
+++ components/newin.js
@@ -4,11 +4,11 @@ import flash from '../icons/flash.svg';
 const Newin = () => {
   return (
     <div className="newin">
-      <div className="d-flex align-items-center justify-content-between sticky-toolbar">
+      <div className="d-flex flex-sm-column align-items-center align-items-sm-start justify-content-between sticky-toolbar">
         <div className="d-flex align-items-center">
           <img src={flash} alt="icon" height="35" /> <h1>New In</h1>
         </div>
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center toolbar">
           <button className="toolbar-button female">
             <span className="icon"></span>{' '}
             <span className="content">Female</span>
@@ -28,48 +28,48 @@ const Newin = () => {
           </button>
         </div>
       </div>
-      <div className="d-flex highlights">
-        <div className="w-50">
-          <div className="category-horizontal-card cyan-background align-items-center text-center font-weight-800 font-size-22 m-1">
+      <div className="d-flex flex-md-column flex-sm-column highlights">
+        <div className="w-50 w-md-100 w-sm-100">
+          <div className="category-horizontal-card cyan-background align-items-center text-center font-weight-800 font-size-22 mt-1 m-sm-0 my-sm-2">
             GET UPTO 50% OFF
           </div>
-          <div className="category-horizontal-card purple-background m-1">
+          <div className="category-horizontal-card purple-background mt-1 m-sm-0 my-sm-2">
             <div className="m-2">
               <p className="font-weight-700 font-size-22">Winter Weekends</p>
               <p className="font-weight-300 font-size-14">Keep it casual</p>
             </div>
           </div>
-          <div className="d-flex">
-            <div className="w-50">
-              <div className="category-vertical-card m-1 scarves">
+          <div className="d-flex flex-sm-column">
+            <div className="w-50 w-sm-100">
+              <div className="category-vertical-card mt-1 m-sm-0 my-sm-2 scarves">
                 <p>
                   Blanket scarf in orange and black brushed check with tassels
                 </p>
               </div>
             </div>
-            <div className="w-50">
-              <div className="category-vertical-card m-1">
+            <div className="w-50 w-sm-100">
+              <div className="category-vertical-card mt-1 m-sm-0 my-sm-2 adidas">
                 <p>Adidas Originals LXCON 94 trainers in white</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="w-50">
-          <div className="d-flex">
-            <div className="w-50">
-              <div className="category-vertical-card m-1 adidas">
+        <div className="w-50 w-md-100 w-sm-100">
+          <div className="d-flex flex-sm-column">
+            <div className="w-50 w-sm-100">
+              <div className="category-vertical-card m-1 m-sm-0 my-sm-2 adidas">
                 <p>Adidas Originals LXCON 94 trainers in white</p>
               </div>
             </div>
-            <div className="w-50">
-              <div className="category-vertical-card m-1 sneakers">
+            <div className="w-50 w-sm-100">
+              <div className="category-vertical-card m-1 m-sm-0 my-sm-2 sneakers">
                 <p>Lightweight super skinny joggers in light khaki</p>
               </div>
             </div>
           </div>
 
-          <div className="category-horizontal-card yellow-background m-1">
+          <div className="category-horizontal-card yellow-background m-1 m-sm-0 my-sm-2">
             <div className="d-flex flex-column m-2">
               <span className="font-weight-700 font-size-22">
                 New-In Knitwear
@@ -79,7 +79,7 @@ const Newin = () => {
               </span>
             </div>
           </div>
-          <div className="category-horizontal-card pink-background m-1">
+          <div className="category-horizontal-card pink-background m-1 m-sm-0 my-sm-2">
             <div className="m-2">
               <p className="font-weight-700 font-size-22">
                 New-Season Textures
