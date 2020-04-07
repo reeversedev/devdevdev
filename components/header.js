2b248242f88a3d8c16c297c630301d7e99739ad8
Prateek Gogia <gogia.prateek@hotmail.com>
2020-04-07 21:07:06 +0530

    Hello name text animation is there now

--- components/header.js
+++ components/header.js
@@ -24,26 +24,22 @@ const Header = () => {
         />
       </label>
 
-      <div className="d-flex justify-content-between align-items-center">
+      <div className="d-flex justify-content-between align-items-center cart">
         <img
           src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"
           alt=""
           height="16"
-          className="cart"
         />
         <span>Cart: </span>
         <span>2</span>
       </div>
       <div className="d-flex w-sm-100">
         <div className="hello-name">
-          <a>
-            <img
-              src="https://pbs.twimg.com/profile_images/1239922488160575489/_Ykuf9DR_400x400.jpg"
-              alt="Profile Picture"
-              height="30"
-            />
-          </a>
-
+          <img
+            src="https://pbs.twimg.com/profile_images/1239922488160575489/_Ykuf9DR_400x400.jpg"
+            alt="Profile Picture"
+            height="30"
+          />
           <span>
             Hello, <b>Mr. Prateek</b>
           </span>
