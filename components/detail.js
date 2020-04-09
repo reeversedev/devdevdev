a400b33cbf7d8e863d496fa61603b1a12cf70068
Prateek Gogia <gogia.prateek@hotmail.com>
2020-04-09 20:41:54 +0530

    Added some sections of details

--- components/detail.js
+++ components/detail.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
 
+const specifications = [
+  { heading: 'Type', description: 'Sneakers' },
+  { heading: 'Toe Shape', description: 'Round Toe' },
+  { heading: 'Pattern', description: 'Colourblocked' },
+  { heading: 'Fastening', description: 'Lace-Ups' },
+  { heading: 'Shoe Width', description: 'Wide' },
+  { heading: 'Ankle Height', description: 'Regular' },
+  { heading: 'Insole', description: 'Comfort Insole' },
+  { heading: 'Sole Material', description: 'Rubber' },
+  { heading: 'Micro Trend', description: 'Contrast Sole' },
+  { heading: 'Warranty', description: '3 months' },
+];
+
 const Detail = () => {
   const [quantity, setQuantity] = useState(0);
 
@@ -26,12 +39,66 @@ const Detail = () => {
         </div>
         <hr className="my-2" />
         <div className="d-flex justify-content-between align-items-center">
-          <button className="transaction buynow">Buy now</button>
+          <button className="transaction add-to-cart">Add to Cart </button>
+          {/* Will add this in Cart Menu */}
+          {/* <button className="transaction buynow">Buy now </button> */}
+
           <div className="button-group">
             <button className="minus">-</button>
             <span>{quantity}</span>
             <button className="plus">+</button>
           </div>
+        </div>
+        <div className="d-flex flex-column mt-2">
+          <h4>Product Details</h4>
+
+          <section>
+            <p className="font-weight-700">Special technologies</p>
+            <p>Responsive Boost midsole Lightweight, supportive feel</p>
+          </section>
+
+          <section>
+            <p className="font-weight-700">
+              About ADIDAS Original Men Nite Jogger Sneakers
+            </p>
+            <p>
+              The first Nite Jogger debuted in 1980, when recreational running
+              was becoming popular. True to their origins, these shoes flash
+              reflective details. Made of a mix of nylon, mesh and leather, they
+              show off wintry silver hues. Boost cushioning adds a responsive,
+              energy-returning feel.
+            </p>
+          </section>
+
+          <section>
+            <p className="font-weight-700">Product design details</p>
+            <p>
+              Main materials: mesh and synthetic upper/ textile lining/ rubber
+              outsole Brand colour: -ROYBLU/GREONE/FTWWHT Lace-up closure
+              Colourblocked upper Warranty: 3 months Warranty provided by brand/
+              manufacturer
+            </p>
+          </section>
+
+          <section>
+            <p className="font-weight-700">Material & Care</p>
+            <p>Mesh and synthetic Wipe with a clean, dry cloth when needed</p>
+          </section>
+          <section>
+            <p className="font-weight-700">Specifications</p>
+            <div className="d-flex flex-wrap justify-content-between">
+              {specifications.map(({ heading, description }) => {
+                return (
+                  <div className="w-50">
+                    <p className="font-size-12 font-weight-200 text-muted">
+                      {heading}
+                    </p>
+                    <p className="font-weight-400">{description}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </section>
         </div>
       </div>
     </div>
