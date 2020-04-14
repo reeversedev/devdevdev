8d2abfe05c2eb787f96c8b73e96761189e292a8d
Prateek Gogia <gogia.prateek@hotmail.com>
2020-04-14 14:20:37 +0530

    Large screen homepage looks better

--- components/newin.js
+++ components/newin.js
@@ -29,40 +29,42 @@ const Newin = () => {
         </div>
       </div>
       <div className="d-flex flex-md-column flex-sm-column highlights">
-        <div className="w-50 w-md-100 w-sm-100">
-          <div className="category-horizontal-card cyan-background align-items-center text-center font-weight-800 font-size-22 mt-1 m-sm-0 my-sm-2">
-            GET UPTO 50% OFF
-          </div>
-          <div className="category-horizontal-card purple-background mt-1 m-sm-0 my-sm-2">
-            <div className="m-2">
-              <p className="font-weight-700 font-size-22">Winter Weekends</p>
-              <p className="font-weight-300 font-size-14">Keep it casual</p>
+        <div className="w-50 w-md-100 w-sm-100 p-1">
+          <section className="vertical-section my-2">
+            <div className="category-horizontal-card cyan-background align-items-center text-center font-weight-800 font-size-22 m-sm-0 my-sm-2">
+              GET UPTO 50% OFF
             </div>
-          </div>
-          <div className="d-flex flex-sm-column">
-            <div className="category-vertical-card m-1 m-sm-0 my-sm-2 scarves">
+            <div className="category-horizontal-card purple-background m-sm-0 my-sm-2">
+              <div className="m-2">
+                <p className="font-weight-700 font-size-22">Winter Weekends</p>
+                <p className="font-weight-300 font-size-14">Keep it casual</p>
+              </div>
+            </div>
+          </section>
+          <section className="horizontal-section">
+            <div className="category-vertical-card m-sm-0 my-sm-2 scarves">
               <p>
                 Blanket scarf in orange and black brushed check with tassels
               </p>
             </div>
-            <div className="category-vertical-card m-1 m-sm-0 my-sm-2 adidas">
+            <div className="category-vertical-card m-sm-0 my-sm-2 adidas">
               <p>Adidas Originals LXCON 94 trainers in white</p>
             </div>
-          </div>
+          </section>
         </div>
 
-        <div className="w-50 w-md-100 w-sm-100">
+        <div className="w-50 w-md-100 w-sm-100 p-1">
           <div className="d-flex flex-column flex-sm-column">
-            <div className="d-flex flex-sm-column">
-              <div className="category-vertical-card m-1 m-sm-0 my-sm-2 adidas">
+            <section className="horizontal-section my-2">
+              <div className="category-vertical-card m-sm-0 my-sm-2 adidas">
                 <p>Adidas Originals LXCON 94 trainers in white</p>
               </div>
-              <div className="category-vertical-card m-1 m-sm-0 my-sm-2 sneakers">
+              <div className="category-vertical-card m-sm-0 my-sm-2 sneakers">
                 <p>Lightweight super skinny joggers in light khaki</p>
               </div>
-            </div>
-            <div className="d-flex flex-column">
-              <div className="category-horizontal-card yellow-background m-1 m-sm-0 my-sm-2">
+            </section>
+            <section className="vertical-section">
+              <div className="category-horizontal-card yellow-background m-sm-0 my-sm-2">
                 <div className="d-flex flex-column m-2">
                   <span className="font-weight-700 font-size-22">
                     New-In Knitwear
@@ -72,7 +74,7 @@ const Newin = () => {
                   </span>
                 </div>
               </div>
-              <div className="category-horizontal-card pink-background m-1 m-sm-0 my-sm-2">
+              <div className="category-horizontal-card pink-background m-sm-0 my-sm-2">
                 <div className="m-2">
                   <p className="font-weight-700 font-size-22">
                     New-Season Textures
@@ -80,7 +82,7 @@ const Newin = () => {
                   <p className="font-weight-300 font-size-14">Change is here</p>
                 </div>
               </div>
-            </div>
+            </section>
           </div>
         </div>
       </div>
