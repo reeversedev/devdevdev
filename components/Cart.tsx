35bbc4e52f89e8b6d8ae67509def7a7b526fd45b
Prateek Gogia <gogia.prateek@hotmail.com>
2020-06-12 22:44:58 +0530

    Added volume and color

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -11,7 +11,7 @@ const Cart = () => {
   return (
     <div className="cart">
       <div className="cart-header d-flex justify-content-between align-items-center">
-        <h1>Cart</h1>
+        <h1>My Cart</h1>
         <div className="d-flex align-items-center">
           <span
             className="cursor-pointer cart-icon cart-size"
@@ -26,45 +26,53 @@ const Cart = () => {
         </div>
       </div>
       <div className="cart-items-container d-flex">
-        <div className="w-60 w-sm-100">
-          <div className="items">
-            {products.clothing.map((cloth, index) => {
-              return (
-                <div className="item" key={index}>
-                  <div
-                    className="w-20"
-                    style={{
-                      backgroundImage: `url('${cloth.image}')`,
-                      backgroundSize: '100% 100%',
-                      height: '150px',
-                      borderRadius: '15px',
-                    }}
-                  ></div>
-                  <div className="description w-100">
-                    <p className="brand">{cloth.brand}</p>
-                    <p className="name">{cloth.name}</p>
-                  </div>
-                  <div className="d-flex flex-column w-20 p-1">
-                    <p className="price">&#8377; {cloth.price}</p>
-                    <div className="quantity">
-                      <div className="button-group">
-                        <button className="minus">-</button>
-                        <span>2</span>
-                        <button className="plus">+</button>
-                      </div>
+        <div className="items">
+          {products.clothing.map((cloth, index) => {
+            return (
+              <div className="item align-items-center" key={index}>
+                <div
+                  className="w-20"
+                  style={{
+                    backgroundImage: `url('${cloth.image}')`,
+                    backgroundSize: '100% 100%',
+                    height: '150px',
+                    width: '200px',
+                    borderRadius: '15px',
+                  }}
+                ></div>
+                <div className="description w-100">
+                  <p className="brand">{cloth.brand}</p>
+                  <p className="name">{cloth.name}</p>
+                  <p className="volume">Size: XL • Color: White</p>
+                </div>
+                <div className="w-20">
+                  <p className="price">&#8377; {cloth.price}</p>
+                </div>
+                <div className="d-flex flex-column w-20 p-1">
+                  <div className="quantity">
+                    <div className="button-group">
+                      <button className="minus">-</button>
+                      <span>2</span>
+                      <button className="plus">+</button>
                     </div>
                   </div>
                 </div>
-              )
-            })}
-          </div>
-        </div>
-        <div className="w-40 w-sm-100">
-          <div className="cart-detail"></div>
+                <div className="w-20">
+                  <p className="price text-center">
+                    &#8377; {parseFloat(cloth.price) * 2}
+                  </p>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
       <div className="cart-footer">
-        <a className="transaction buynow text-uppercase">Checkout</a>
+        {/* {products.clothing.reduce(
+          (acc, { price }, i) => acc + parseInt(price) * 2,
+          0
+        )} */}
+        <a className="transaction buynow text-uppercase">Checkout </a>
       </div>
     </div>
   )
