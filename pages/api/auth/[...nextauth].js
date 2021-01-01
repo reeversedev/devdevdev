036dc9af8245c2eec014998888989bd81882be40
Prateek Gogia <gogia.prateek@hotmail.com>
2021-01-01 13:33:07 +0530

    authentication middleware

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+}
+
+export default (req, res) => NextAuth(req, res, options)
