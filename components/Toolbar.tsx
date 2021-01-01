35301bb9a71c2ceaf15b21721b518f446098d2af
Prateek Gogia <gogia.prateek@hotmail.com>
2021-01-01 17:34:52 +0530

    Gender toggle button (#17)
    * added spans for genders
    
    * fixed layout issues
    
    * alignment issues fix
    
    * classname changes

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,5 +1,34 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from '../pages/_app'
+
+const GenderToggleButton = () => {
+  const [gender, setGender] = useState(false)
+  return (
+    <>
+      <input
+        type="checkbox"
+        name="gender-toggle"
+        id="gender-toggle"
+        className="toggle-container toggle-container-light"
+        onChange={() => setGender(!gender)}
+      />
+      <label htmlFor="gender-toggle" className="toggle-button">
+        <span
+          id="female"
+          className="d-flex align-items-center justify-content-center"
+        >
+          <img src="/women.svg" alt="women" width="18" /> Women
+        </span>
+        <span
+          id="male"
+          className="d-flex align-items-center justify-content-end"
+        >
+          <img src="/men.svg" alt="men" width="18" /> Men
+        </span>
+      </label>
+    </>
+  )
+}
 
 const Toolbar = ({ buttons }) => {
   const {
@@ -8,10 +37,7 @@ const Toolbar = ({ buttons }) => {
   } = useContext(UserContext)
   return (
     <div className="d-flex justify-content-between align-items-center toolbar">
-      {/* {buttons.map((button, index) => (
-        <ToolbarButton key={index} {...button} />
-      ))} */}
-      {/* <p>Toolbar button issue here</p> */}
+      <GenderToggleButton />
     </div>
   )
 }
