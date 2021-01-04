de68f10cb99b94116473c518f1712a77fb3370cd
Kanishk Gupta <g.kanishk@holosuit.com>
2021-01-04 07:10:02 +0530

    Default URL redirect fix(#22)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
 import Layout from '../components/Layout'
 import Category from './category/[slug]'
 
 const Home = () => {
+  useEffect(() => {
+    const { pathname } = Router
+    if (pathname == '/') {
+      Router.push('/category/new-in')
+    }
+  })
+
   return <Layout>{/* <Category /> */}</Layout>
 }
 
